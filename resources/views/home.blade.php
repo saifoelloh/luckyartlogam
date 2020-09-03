0544cabb419c68d711f0ddadd8d82e6168cbc4d7
rudikurniawan99 <rudikurniawan501@gmail.com>
2020-09-04 01:28:27 +0700

    add benefit section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,60 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Fluid jumbotron</h1>
+    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  </div>
+</div>
+<section class="benefit-section">
+  <div class="container">
+    <h1 class="title-section text-center">Benefits</h1>
+    <div class="row">
+      <div class="col-12 col-lg-6 my-3">
+        <div class="card border-0 bg-transparent px-5">
+          <img src="{{ asset('assets/icon-real-pict.png') }}" class="mx-auto" alt="...">
+          <div class="card-body text-center">
+            <h5 class="card-title">Real Pict</h5>
+            <p class="card-text">Hasil pesanan sesuai dengan apa yang kami berikan di foto produk. Kami menjaga kepercayaan anda sepenuhnya</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6 my-3">
+        <div class="card border-0 bg-transparent px-5">
+          <img src="{{ asset('assets/icon-tanpa-min.png') }}" class="mx-auto" alt="...">
+          <div class="card-body text-center">
+            <h5 class="card-title">Tanpa Minimum Transaksi</h5>
+            <p class="card-text">Bebas untuk menentukan pesanan dalam jumah besar maupun satuan. Kami tidak membatasi jumlah order</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6 my-3">
+        <div class="card border-0 bg-transparent px-5">
+          <img src="{{ asset('assets/icon-custom-design.png') }}" class="mx-auto" alt="...">
+          <div class="card-body text-center">
+            <h5 class="card-title">Custom Design</h5>
+            <p class="card-text">Dapat menentuan design anda sendiri, kami akan mengerjakan pesanan sesuai dengan apa yang anda ingkan.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6 my-3">
+        <div class="card border-0 bg-transparent px-5">
+          <img src="{{ asset('assets/icon-sdm-unggul.png') }}" class="mx-auto" alt="...">
+          <div class="card-body text-center">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Didukung oleh SDM yang handal, dan akan memastikan hasil pesanan terjamin mutu dan kualitasnya</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</section>
+  
+
+  
   <div class="row justify-content-center">
     <div class="col-md-8">
       <div class="card">
@@ -19,5 +72,4 @@
       </div>
     </div>
   </div>
-</div>
 @endsection
